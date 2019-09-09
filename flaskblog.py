ef50174d9f98fc57a7672c747e3101f7e81b3845
Rolly Monsanto <rolly.monsanto@gmail.com>
2019-09-09 21:56:33 +1000

    2

--- flaskblog.py
+++ flaskblog.py
@@ -1,9 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
+app = Flask(__name__)
 
-app=Flask(__name__)
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
 
 
-@app.route('/')
-def index():
-    return 'Hello Rolly'
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
 
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
