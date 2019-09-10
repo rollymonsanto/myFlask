2620b4d0d9119de329659498156465fda5d628e6
Rolly Monsanto <rolly.monsanto@gmail.com>
2019-09-10 23:27:15 +1000

    3

--- flaskblog.py
+++ flaskblog.py
@@ -1,5 +1,10 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
+
+
 app = Flask(__name__)
+
+app.config['SECRET_KEY']='b02738ec80cbbce9ff5a2c5c75866197'
 
 posts = [
     {
@@ -26,6 +31,18 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
+
+
+@app.route("/register")
+def register():
+    form=RegistrationForm()
+    return render_template('register.html', title='Register',form=form)
+
+
+@app.route("/login")
+def login():
+    form=LoginForm()
+    return render_template('login.html', title='Login',form=form)
 
 
 if __name__ == '__main__':
